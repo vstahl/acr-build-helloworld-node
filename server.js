4731c895b6551dd4248c9f0bbe6382a49eb69cf4
Volker Stahl <vstahl@microsoft.com>
2020-06-14 19:56:54 +0200

    Changed to Tom

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello Peter\n')
+  response.write('Hello Tom\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
